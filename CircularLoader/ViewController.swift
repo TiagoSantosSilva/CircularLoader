c5fda58a328e916d89429800d99087ae85001935
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-11 17:26:14 +0000

    Added the stroke color and it's filling.

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let shapeLayer = CAShapeLayer()
-        
         let circularPath = UIBezierPath(arcCenter: view.center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        createProgressCircle(circularPath)
+    }
+    
+    private func createProgressCircle(_ circularPath: UIBezierPath) {
         shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        shapeLayer.lineWidth = 10
+        shapeLayer.strokeEnd = 0
         
         view.layer.addSublayer(shapeLayer)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    @objc private func handleTap() {
+        print("Attempting to animate stroke")
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
     }
 }
 
