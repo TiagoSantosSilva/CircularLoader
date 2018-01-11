f43ef52e1bfba5191c0811ff890e9742b6492e81
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-11 17:18:49 +0000

    Creation of the circle in the center.

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let shapeLayer = CAShapeLayer()
+        
+        let circularPath = UIBezierPath(arcCenter: view.center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        
+        view.layer.addSublayer(shapeLayer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
