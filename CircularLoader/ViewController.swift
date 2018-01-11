1738f82f113177e068c24e3c71b2db5465a3b510
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-11 17:55:01 +0000

    Clicking the screen now makes the shape layer "load".

--- CircularLoader/ViewController.swift
+++ CircularLoader/ViewController.swift
@@ -10,26 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let shapeLayer = CAShapeLayer()
+    var progressShapeLayer: CAShapeLayer!
+    var trackLayer: CAShapeLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let circularPath = UIBezierPath(arcCenter: view.center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
         
-        createProgressCircle(circularPath)
+        trackLayer = createProgressCircle(circularPath, strokeColor: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), strokeEnd: 1)
+        progressShapeLayer = createProgressCircle(circularPath, strokeColor: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), strokeEnd: 0)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
-    private func createProgressCircle(_ circularPath: UIBezierPath) {
+    private func createProgressCircle(_ circularPath: UIBezierPath, strokeColor: CGColor, strokeEnd: CGFloat) -> CAShapeLayer {
+        let shapeLayer = CAShapeLayer()
         shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        shapeLayer.strokeColor = strokeColor
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineWidth = 10
         shapeLayer.lineCap = kCALineCapRound
-        shapeLayer.strokeEnd = 0
+        shapeLayer.strokeEnd = strokeEnd
         
         view.layer.addSublayer(shapeLayer)
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        return shapeLayer
     }
     
     @objc private func handleTap() {
@@ -42,7 +47,7 @@ class ViewController: UIViewController {
         basicAnimation.fillMode = kCAFillModeForwards
         basicAnimation.isRemovedOnCompletion = false
         
-        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+        progressShapeLayer.add(basicAnimation, forKey: "urSoBasic")
     }
 }
 
